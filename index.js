6a7a96c28a1799bffa93ea98d31608cb23cb19fa
the1nekev <kevcorcino15@gmail.com>
2023-06-08 07:57:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -203,13 +203,13 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false}),
     }
 
     let hashedPassword = Users.hashPassword(req.body.Password);
-    
+
 	Users.findOneAndUpdate(
 		{ Username: req.params.Username },
 		{
 			$set: {
 				Username: req.body.Username,
-				Password: req.body.Password,
+				Password: hashedPassword,
 				Email: req.body.Email,
 				Birthday: req.body.Birthday,
 			},
