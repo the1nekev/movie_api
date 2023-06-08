bc6ac330d3d534838ae5ce8983007313ce9b5ada
the1nekev <kevcorcino15@gmail.com>
2023-06-08 07:51:21 -0400

    Data Validation

--- index.js
+++ index.js
@@ -189,7 +189,21 @@ app.post('/users', (req, res) => {
 
 //UPDATE User's info by Username
 app.put('/users/:Username', passport.authenticate('jwt', { session: false}),
+[
+    check('Username', 'Username is required').isLength({ min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required.').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid.').isEmail()
+],
 (req, res) => {
+    let errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({ errors: errors.array});
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    
 	Users.findOneAndUpdate(
 		{ Username: req.params.Username },
 		{
