d72403c27479193582a6a3b753fff925ac1137f7
the1nekev <kevcorcino15@gmail.com>
2023-06-09 09:33:09 -0400

    Wildcard

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-let allowedOrigins = ['http://localhost:8080','http://testsite.com','*'];
+let allowedOrigins = ['*'];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
